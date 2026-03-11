e62b74cda098f98ef188af518fd1b494e03291d2
Berat Zengin <devberatzengin@gmail.com>
2026-03-11 16:20:46 +0300

    feat: add /me endpoint to UserController for authenticated profile retrieval

--- src/main/java/org/devberat/controller/Impl/UserControllerImpl.java
+++ src/main/java/org/devberat/controller/Impl/UserControllerImpl.java
@@ -5,6 +5,7 @@ import org.devberat.DTO.*;
 import org.devberat.model.RootEntity;
 import org.devberat.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +14,11 @@ public class UserControllerImpl extends RestBaseController {
     @Autowired
     private IUserService userService;
 
+    // Post's
     @PostMapping("/save")
     public RootEntity<CreateUserResponse> saveUser(@Valid @RequestBody CreateUserRequest request) {
         return ok(userService.saveUser(request));
     }
-
 
     @PostMapping("/activate")
     public RootEntity<ActivateUserResponse> activateUser(@Valid @RequestBody ActivateUserRequest request) {
@@ -27,5 +28,12 @@ public class UserControllerImpl extends RestBaseController {
     @PostMapping("/deactivate")
     public RootEntity<InActiveUserResponse> deactivateUser(@Valid @RequestBody InActiveUserRequest request){
         return ok(userService.inActiveUser(request));
+    }
+
+
+    // Get's
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getMyProfile() {
+        return ResponseEntity.ok(userService.getMyProfile());
     }
 }
