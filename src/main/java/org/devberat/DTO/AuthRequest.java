ed1604298d8c8153e831d95064315b14bf5ce705
Berat Zengin <devberatzengin@gmail.com>
2026-03-11 15:37:01 +0300

    feat: create auth DTOs for login and registration requests

--- src/main/java/org/devberat/DTO/AuthRequest.java
+++ src/main/java/org/devberat/DTO/AuthRequest.java
@@ -1,4 +1,23 @@
 package org.devberat.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
+
+    @NotEmpty
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @NotEmpty
+    private String password;
 }
