d0d88e515ca6e44985217b56d82f10e417eea7c4
Berat Zengin <devberatzengin@gmail.com>
2026-03-19 16:19:36 +0300

    config: enable spring scheduling for the application

--- src/main/java/org/devberat/starter/FlightTrackerApplication.java
+++ src/main/java/org/devberat/starter/FlightTrackerApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Schedules;
 
 @EntityScan( basePackages = {"org.devberat"})
 @EnableJpaRepositories( basePackages = {"org.devberat"})
 @ComponentScan(basePackages = {"org.devberat"})
 @SpringBootApplication
+@EnableScheduling
 public class FlightTrackerApplication {
 
     public static void main(String[] args) {
